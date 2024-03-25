2c97920d3f41b5e155d9de62ec0306ee1800d0fe
joseneisantana <joseneisantana@yahoo.com>
2024-03-25 17:09:30 -0300

    Exercicio

---
+++ src/Exercicio1/ClassCar.java
@@ -0,0 +1,18 @@
+package Exercicio1;
+
+public class ClassCar {
+
+	public static void main(String[] args) {
+
+		String cor = "Azul";
+		String modelo = "Porsche Panameira";
+		String rodas ="Aro 20";
+		int ano = 2022;
+		
+		System.out.println("*************************");
+		System.out.println("Dados do Carro");
+		System.out.println(modelo +" " + cor + " "+ rodas + " "+" Ano" + ano);
+
+	}
+
+}
